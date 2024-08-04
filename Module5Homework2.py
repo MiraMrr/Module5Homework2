20277d1a19801687136fceb665e8a6754830c9fa
Karina <psinka.94@gmail.com>
2024-08-04 13:45:43 +0300

    Add files via upload

---
+++ Module5Homework2.py
@@ -0,0 +1,27 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if 1 <= new_floor <= self.number_of_floors:
+            for i in range(1, new_floor + 1):
+                print(i)
+        else:
+            print('Такого этажа не существует')
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+
+House1 = House('ЖК Лесной', 5)
+House2 = House('Столица', 25)
+
+print(House1)
+print(House2)
+
+print(len(House1))
+print(len(House2))
